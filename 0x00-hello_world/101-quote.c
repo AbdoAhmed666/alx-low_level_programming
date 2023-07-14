5881155d31786afcf9c86a354ceda966f7bef0e0
Abdo <abdelrhman555@gmail.com>
2023-07-13 18:19:50 -0700

    advance2 commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <unistd>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,9 +10,8 @@
 */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(1,quo,59);
+	write(1, quo, 59);
 	return (1);
 }
