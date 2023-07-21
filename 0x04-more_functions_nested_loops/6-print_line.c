c2ba6c9ed4e9c0b4f6700bd38ad3c83bf2b9d3a3
Abdo <abdelrhman555@gmail.com>
2023-07-21 05:09:01 -0700

    s commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,11 +3,13 @@
  * Write a function that draws a straight line in the terminal
  *
  * Where n is the number of times the character _ should be printed
- *
+ *@n:parameter
  * If n is 0 or less, the function should only print \n
+ *
+ * Return: nothing
 */
 
-void print_line(int n);
+void print_line(int n)
 {
 	if (n <= 0)
 	{
