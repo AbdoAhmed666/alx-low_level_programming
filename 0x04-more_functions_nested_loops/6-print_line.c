934b6effc8e30681135e6f33d76481ac2f91c06f
Abdo <abdelrhman555@gmail.com>
2023-07-21 05:11:06 -0700

    s commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,16 +11,9 @@
 
 void print_line(int n)
 {
-	if (n <= 0)
-	{
-		_putchar("\n");
-	}
-	else
-	{
-		for (, n > 0, n--)
-		{
-			_putchar("-");
-		}
-		Putchar("\n");
-	}
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
